6bb4c59f8d913edb0bdc3ddf24f8e04e4746cfcc
Anton Gritsenko <anton.gritsenko@itnetx.ch>
2020-03-04 07:14:05 +0300

    OOB template from VS 2019 16.4.4, .net core 3.1.1, Angular 8.2.12

---
+++ webAppNetCore3.1SPAIS4/Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace webAppNetCore3._1SPAIS4.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
